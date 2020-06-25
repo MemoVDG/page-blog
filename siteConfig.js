c1e70dd1b79ca875d22c7cc464f8a47ce6e9ca62
Memo Vara <memovdg@gmail.com>
2020-06-24 21:57:38 -0500

    Initial commit

---
+++ siteConfig.js
@@ -0,0 +1,12 @@
+module.exports = {
+    siteUrl: `https://memovdg.herokuapp.com`,
+    postsPerPage: 12,
+    siteTitleMeta: `Memo VDG Blog`,
+    siteDescriptionMeta: `Mi pagina y blog personal`,
+    shareImageWidth: 1000,
+    shareImageHeight: 523,
+    shortTitle: `MemoVDG`,
+    siteIcon: `favicon.png`,
+    backgroundColor: `#e9e9e9`,
+    themeColor: `#15171A`,
+};
